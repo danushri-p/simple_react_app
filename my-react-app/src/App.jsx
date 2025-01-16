038a29495e3bba0f16a65846c21d32347b59dcce
Danushri Prakash <danushri.saranyaprakash@gmail.com>
2025-01-16 16:27:19 +0530

    newest

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Name: Danushri Prakash</h1>
+      <p>contact: danushri.saranyaprakash@gmail.com</p>
+    </div>
+  );
+};
+
+export default App;
